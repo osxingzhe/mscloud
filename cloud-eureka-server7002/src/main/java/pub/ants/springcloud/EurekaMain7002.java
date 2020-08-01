dfba61f94607ee56e823a23d715a2dc62b7729d6
iearl <dc_magaowei@163.com>
2020-08-01 15:57:43 +0800

    eureka集群

---
+++ cloud-eureka-server7002/src/main/java/pub/ants/springcloud/EurekaMain7002.java
@@ -0,0 +1,20 @@
+package pub.ants.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author magw
+ * @version 1.0
+ * @date 2020/8/1 下午1:29
+ * @description: No Description
+ *   @EnableEurekaServer  表明是注册中心
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class,args);
+    }
+}
